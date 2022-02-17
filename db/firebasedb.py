c7cbfc0b0c7456d7e133be845b234d94cd54e3a8
adityaag121 <adityaag121@gmail.com>
2022-02-17 22:16:57 +0530

    removed firebase credentials from codebase

--- db/firebasedb.py
+++ db/firebasedb.py
@@ -1,11 +1,12 @@
-import os   
+import os
+from ast import literal_eval
 
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import db
 
 def get_reference():
-    cred = credentials.Certificate(os.getenv('FIREBASE_CREDENTIAL'))
+    cred = credentials.Certificate(literal_eval(os.getenv('FIREBASE_CREDENTIAL')))
     firebase_admin.initialize_app(cred, {'databaseURL':os.getenv('DATABASE_URL')})
 
     ref = db.reference("/books")
